f8e63e11767ab5ebc639c79c4be93668de5773ba
Chris Monzert <cmonzert@gmail.com>
2020-06-29 18:44:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
+def fizzbuzz
+  if fizz_3 == fizzbuzz(3)
+    puts "Fizz"
+  
+  elsif fizz_5 == fizzbuzz(5)
+
+   puts "Buzz"
+  elsif fizz_15 == fizzbuzz(15)
+  
+   puts "FizzBuzz"
+  else
+    puts "nil"
+    end
