050a6526477ec5bcf4416d283435d364efc1d740
Chris Monzert <cmonzert@gmail.com>
2020-06-29 19:01:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,5 @@
 
-def fizzbuzz
-  if fizz_3 == fizzbuzz(3)
-    puts "Fizz"
-  elsif fizz_5 == fizzbuzz(5)
-    puts "Buzz"
-  elsif fizz_15 = fizzbuzz(15)
-    puts "FizzBuzz"
-  elsif fizz_4 = fizzbuzz(4)
-    puts "nil"
+def fizzbuzz(int)
+  if int % 3 && int % 5
 end
 
